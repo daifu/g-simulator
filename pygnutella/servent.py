73eae3c53e06ca2bb2dd4a803928aad5f374e6c5
taipham <kael136@gmail.com>
2012-02-19 11:14:31 -0800

    add some basic interface ti servent.py

--- pygnutella/servent.py
+++ pygnutella/servent.py
@@ -20,3 +20,30 @@ class Servent:
         docstring for setFiles
         """
         self.files = files
+
+"""** TODO **"""
+"""** set and get methods for: hostName/ip, portNum, nums_files_share **"""
+"""** and array_of_file_share **"""
+"""** 1 more get and set method for something called sockPool (socketPool) **"""
+"""** as Howard want **"""
+
+  def CreateMessage(self, body):
+    """ TODO (whoever do it is fine)"""
+    """ creating a message, not sure if the input argument is enough """
+  
+  def OnConnect(self, peerID):
+    """ TODO (my part)"""
+    """ what to do when a servent connect to a network """
+
+  def OnReceive(self, peerID, message):
+    """ TODO (my part) """
+    """ servent behavior when receiving a message """
+
+  def OnDisconnect(self, peerID):
+    """ TODO (my part) """
+    """ servent behavior when leaving the network """
+
+  def OnError(self, peerID):
+    """ TODO (my part) """
+    """ servent behavior when timeout and/or pause message """
+  

777d6d8d31d9e1972d6bd94348fc40f7898d4d04
taipham <kael136@gmail.com>
2012-02-19 19:02:40 -0800

    add few line into servent.py

--- pygnutella/servent.py
+++ pygnutella/servent.py
@@ -3,6 +3,15 @@
 # port number.
 #
 
+import logging;
+import Reactor;
+
+PIND_ID = 0x00
+POND_ID = 0x01
+QUERY_ID = 0x80
+QUERYHIT_ID = 0x81
+PUSH_ID = 0x40
+
 class Servent:
     """
     Basic class for the servent
@@ -24,13 +33,35 @@ class Servent:
     """** TODO **"""
     """** set and get methods for: hostName/ip, portNum, nums_files_share **"""
     """** and array_of_file_share **"""
-    """** 1 more get and set method for something called sockPool (socketPool) **"""
+    """** 1 more get and set method for something called Reactor (socketPool) **"""
     """** as Howard want **"""
 
-    def CreateMessage(self, body):
+    def CreateMessage(self, peer_id, messageID, message):
         """ TODO (whoever do it is fine)"""
         """ creating a message, not sure if the input argument is enough """
+        """ Why pytho don't have switch-case -__- """
+        # this method need to be rechecked, since I just do it by using
+        # the interface provided by other class
+        #
+        #
+        
+        if (messageID = PING_ID):
+            body = PingBody(message)
+        else if (messageID = PONG_ID):
+            body = PongBody(message)
+        else if (messageID = QUERY_ID):
+            body = QueryBody(message)
+        else if (messageID = QUERYHIT_ID):
+            body = QueryHitBody(message)
+        else if (messageID = PUSH_ID):
+            body = PushBody(message)
 
+        ttl = 7
+        self.message = Message(7, 0)
+        self.message.setBody(body)
+        self.reactor = Reactor(self.ip)
+        self.reactor.send(peer_id, message)
+        
     def OnConnect(self, peerID):
         """ TODO (my part)"""
         """ what to do when a servent connect to a network """

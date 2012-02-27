a7148ac242006bc8866b7bae3fb2daa12c8da436
Khai Nguyen <khaing211@gmail.com>
2012-02-26 21:43:56 -0800

    fixed reactor_tests.py because now we are using list instead of dictionary to prevent collision replacement

--- tests/reactor_tests.py
+++ tests/reactor_tests.py
@@ -5,7 +5,7 @@ def setup_func():
     global reactor
     address = ('127.0.0.1', 9990)
     reactor = Reactor(address)
-    assert_not_equal(reactor.channels, {})
+    assert_not_equal(reactor.channels, [])
 
 @with_setup(setup_func)
 

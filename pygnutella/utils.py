6ace2abdfa60ae8eb5e0d56b6ba0c9ec762350f5
Khai Nguyen <khaing211@gmail.com>
2012-02-26 23:44:10 -0800

    add utils, some useful utility. To understand the usefulness, look at the fixed message_body_tests which will update later

---
+++ pygnutella/utils.py
@@ -0,0 +1,24 @@
+import socket, struct
+
+# Get from: http://code.activestate.com/recipes/66517-ip-address-conversion-functions-with-the-builtin-s/
+
+def dottedQuadToNum(ip):
+    "convert decimal dotted quad string to long integer"
+    return struct.unpack('L',socket.inet_aton(ip))[0]
+
+def numToDottedQuad(n):
+    "convert long int to dotted quad string"
+    return socket.inet_ntoa(struct.pack('L',n))
+      
+def makeMask(n):
+    "return a mask of n bits as a long integer"
+    return (2L<<n-1)-1
+
+def ipToNetAndHost(ip, maskbits):
+    "returns tuple (network, host) dotted-quad addresses given IP and mask size"
+    # (by Greg Jorgensen)
+    n = dottedQuadToNum(ip)
+    m = makeMask(maskbits)
+    host = n & m
+    net = n - host
+    return numToDottedQuad(net), numToDottedQuad(host)

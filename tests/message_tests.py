0ec21cdf8ec1371997fb9b3150f21b37dc1368c2
Khai Nguyen <khaing211@gmail.com>
2012-02-27 00:54:01 -0800

    update message_tests.py with serialization and deserialization of Message with PingBody i.e. empty body

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -1,11 +1,25 @@
 from nose.tools import *
 from pygnutella.message import Message
+from pygnutella.messagebody import PingBody
 
 def test_message():
     # Test the default init
-    msg = Message('')
+    msg = Message(message_id = '')    
     assert_equal(msg.ttl, 7)
     assert_equal(msg.hops, 0)
-    assert_equal(msg.body, None)
     assert_equal(msg.payload_length, None)
     assert_equal(msg.payload_descriptor, None)
+    PingBody(msg)
+    
+    # test serialization
+    expected_message = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x07\x00\x00\x00\x00\x00'
+    ser = msg.serialize()
+    assert_equal(ser, expected_message)
+    
+    # test deserialization
+    new_message = Message()
+    new_message.deserialize(expected_message)
+    assert_equal(msg.ttl, 7)
+    assert_equal(msg.hops, 0)
+    assert_equal(msg.payload_length, 0)
+    assert_equal(msg.payload_descriptor, 0)    

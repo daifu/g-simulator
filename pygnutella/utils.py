3e96b2c385d69c7305db1613cb34faf31d22bbfd
Khai Nguyen <khaing211@gmail.com>
2012-02-28 09:46:20 -0800

    ip address is always in big endian. fixed dotted_quad_num, fixed num_to_dotted_quad

--- pygnutella/utils.py
+++ pygnutella/utils.py
@@ -4,11 +4,11 @@ import socket, struct
 
 def dotted_quad_to_num(ip):
     "convert decimal dotted quad string to long integer"
-    return struct.unpack('L',socket.inet_aton(ip))[0]
+    return struct.unpack('!L',socket.inet_aton(ip))[0]
 
 def num_to_dotted_quad(n):
     "convert long int to dotted quad string"
-    return socket.inet_ntoa(struct.pack('L',n))
+    return socket.inet_ntoa(struct.pack('!L',n))
       
 def make_mask(n):
     "return a mask of n bits as a long integer"

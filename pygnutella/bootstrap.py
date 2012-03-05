0e7e95e46de41214786212c08bb260129bed40c0
Khai Nguyen <khaing211@gmail.com>
2012-03-04 23:55:38 -0800

    start making child process for each node. still working out the detail

--- pygnutella/bootstrap.py
+++ pygnutella/bootstrap.py
@@ -1,7 +1,47 @@
-from multiprocessing import Process, Manager
+from servent import Servent
+from multiprocessing import Process, Pipe
+from scheduler import loop as scheduler_loop
 
-def create_node(servent_class, peer_list):
-    return None
+class Command:
+    """
+    This is a list of command send from "bootstrap" process
+    to other process for setup.
+    """
+    # connect command have following ip and port
+    # CONNECT 127.0.1.1 8934
+    CONNECT = "CONNECT"
+    START = "START"
 
-def create_gnutella_network():
-    return None
+class Event:
+    """
+    This is a list of event
+    
+    All event log begin with ON <event> <other parameters>
+    """
+    # example
+    # ON LISTENNING <id-hex-string> 127.0.1.1 8945
+    LISTENING = "LISTENNING"
+    RECEIVED = "RECEIVED"
+   
+def create_gnutella_node(servent_class, pipe, files):
+    servent = servent_class()
+    # TODO: fixed this
+    pipe.send('ON LISTENNING %s %s %s' % ('myhex_id', '127.0.1.1', '3945'))
+    # TODO: receiving peer list
+    while True:
+        cmd = pipe.recv()        
+        if cmd == 'START':
+            break
+        else:
+            # TODO process command
+            pass        
+    scheduler_loop()
+    pipe.close()
+
+_nodes = []
+
+def create_gnutella_network(num_nodes = 10, adjacent_list, preferred_list = []):
+    for _ in xrange(num_nodes):
+        parent_conn, child_conn = Pipe()
+        p = Process(target = create_gnutella_node, args=(Servent, child_conn,)).start()
+        _nodes.append((parent_conn, p))

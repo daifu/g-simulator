513ee6cf83311b9b0558b73dddef128b4c7e0849
Khai Nguyen <khaing211@gmail.com>
2012-03-06 20:03:20 -0800

    two more dispatcher on top of Input/OutputPipeDispatcher which is used to log

--- pygnutella/asyncpipe.py
+++ pygnutella/asyncpipe.py
@@ -1,7 +1,8 @@
-from asyncore import file_dispatcher
+from asyncore import file_dispatcher, loop
 from sys import stderr
 from errno import EPIPE, EBADF
 from traceback import print_exc
+import os
 
 class PipeDispatcher(file_dispatcher):
     def __init__(self, fh, ignore_broken_pipe=False):
@@ -67,14 +68,20 @@ class InputPipeDispatcher(PipeDispatcher):
     def writable(self):
         return PipeDispatcher.writable(self) and bool(self._data_to_write)
     
+    def write(self, data):
+        self._data_to_write += data
+    
     def handle_write(self):
         # writable as muchas possible
         sent = self.send(self._data_to_write)
         self._data_to_write = self._data_to_write[sent:]
         if not bool(self._data_to_write) and self._close_when_done:
             self.handle_close()
+            
+    def close_when_done(self):
+        self._close_when_done = True
     
-class OutPipeDispatcher(PipeDispatcher):
+class OutputPipeDispatcher(PipeDispatcher):
     def __init__(self, fh, chunk_size=512):
         PipeDispatcher.__init__(self, fh)
         self._received_data = ''
@@ -88,3 +95,44 @@ class OutPipeDispatcher(PipeDispatcher):
     
     def handle_read(self):
         self._received_data += self.recv(self._chunk_size)
+        
+class LogInputPipeDispatcher(InputPipeDispatcher):
+    def __init__(self, fh, name, terminator='\n'):
+        self._name = name+": "
+        InputPipeDispatcher.__init__(self, fh)
+        self._terminator = terminator
+        
+    def log(self, msg, *args, **kwargs):
+        self.write(self._name)
+        self.write(msg.format(*args, **kwargs))
+        self.write(self._terminator)
+        
+    def set_terminator(self, terminator):
+        self._terminator = terminator
+        
+class LogOutputPipeDispatcher(OutputPipeDispatcher):
+    def __init__(self, fh, terminator='\n'):
+        OutputPipeDispatcher.__init__(self, fh)
+        self._terminator = terminator
+        self._len_t = len(terminator)
+
+    def set_terminator(self, terminator):
+        self._terminator = terminator
+        self._len_t = len(terminator)
+
+    def handle_read(self):
+        OutputPipeDispatcher.handle_read(self)
+        if self._terminator in self._received_data:
+            idx = self._received_data.find(self._terminator)
+            self.handle_log(self._received_data[:idx])
+            self._received_data = self._received_data[idx+self._len_t:]
+                        
+    def handle_log(self, msg):
+        # output back to the terminal
+        print msg        
+
+def __example_pipe():
+    rp, wp = os.pipe()
+    InputPipeDispatcher(os.fdopen(wp, 'wb'))
+    OutputPipeDispatcher(os.fdopen(rp, 'rb'))
+    loop()    

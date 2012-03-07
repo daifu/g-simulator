3acf184e91977629e09b950ca4bde7b14dc0f99e
Khai Nguyen <khaing211@gmail.com>
2012-03-06 22:21:22 -0800

    remove example of gnutella network

--- examples/gnutella_network.py
+++
@@ -1,6 +0,0 @@
-from pygnutella.bootstrap import create_gnutella_network
-from pygnutella.servent import Servent
-
-if __name__ == '__main__':
-    # create ten basic servent wit basic bootstrap
-    create_gnutella_network([Servent]*1);

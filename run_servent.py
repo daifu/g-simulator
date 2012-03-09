5640f29d287f73116b26ea798037b65b9d6c150e
Khai Nguyen <khaing211@gmail.com>
2012-03-09 00:03:38 -0800

    change Servent to BasicServent and add usage and parameter num check

--- run_servent.py
+++ run_servent.py
@@ -1,13 +1,19 @@
 from pygnutella.network import create_network
-from pygnutella.servent import Servent
+from pygnutella.servent import BasicServent
 import sys
 
+def usage():
+    print "Please run with <bootstrap ip> <bootstrap port> <num of nodes>"
+
 def main(args):
+    if len(args)<3:
+        usage();
+        return
     ip = args[0]
     port = int(args[1])
     num_node = int(args[2])
     address = (ip, port)
-    create_network([Servent]*num_node, address)
+    create_network([BasicServent]*num_node, address)
 
 if __name__ == '__main__':
     main(sys.argv[1:])

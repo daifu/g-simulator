9b9167a7bb7eab57e6c0f34638863f74ce79ef9d
Khai Nguyen <khaing211@gmail.com>
2012-03-08 23:26:01 -0800

    this is very similar to servent_test, this use to debug

---
+++ examples/ping_pong_example.py
@@ -0,0 +1,52 @@
+from pygnutella.servent import BasicServent, FileInfo
+from pygnutella.message import create_message
+from pygnutella.messagebody import GnutellaBodyId
+import asyncore
+import logging
+
+class SendServent(BasicServent):
+    receive_message = []
+    sent_message = []
+    
+    def on_connect(self, connection_handler):
+        ping_message = create_message(GnutellaBodyId.PING)
+        self.sent_message.append(ping_message)
+        self.log("sending %s", ping_message)
+        self.log("ping size %s", len(ping_message.serialize()))
+        connection_handler.write(ping_message.serialize())
+        connection_handler.write(create_message(GnutellaBodyId.PING).serialize())
+        connection_handler.write(create_message(GnutellaBodyId.PING).serialize())
+        return
+    
+    def on_receive(self, connection_handler, message):
+        self.log("received %s", message)
+        self.receive_message.append(message)
+        BasicServent.on_receive(self, connection_handler, message)
+    
+    def readable(self):
+        self.log("readable()")
+        return True
+
+class ReceiveServent(BasicServent):
+    receive_message = []
+    def on_receive(self, connection_handler, message):
+        self.receive_message.append(message)
+        BasicServent.on_receive(self, connection_handler, message)
+
+    def readable(self):
+        self.log("readable()")
+        return True
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s') 
+    receive_servent = ReceiveServent()
+    send_servent = SendServent()
+    
+    # manually set file
+    receive_servent.set_files([FileInfo(1,"first file", 600),  FileInfo(2,"second file", 2500) , FileInfo(3, "third file", 5000)])    
+    receive_servent.reactor.gnutella_connect(send_servent.reactor.address)
+    
+    try:
+        asyncore.loop()
+    finally:        
+        asyncore.close_all()

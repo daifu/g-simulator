ff372c2e83bf4fc4269c02d48d54496ec0e90bc6
Khai Nguyen <khaing211@gmail.com>
2012-03-10 13:28:04 -0800

    log every function call

--- pygnutella/demo/log_servent.py
+++ pygnutella/demo/log_servent.py
@@ -1,4 +1,34 @@
 from ..servent import BasicServent
 
 class LogServent(BasicServent):
-    pass
+    def on_accept(self):
+        self.log("on_accept()")
+        return BasicServent.on_accept(self)
+    
+    def on_connect(self, connection_handler):
+        self.log("on_connect()")
+        BasicServent.on_connect(self, connection_handler)
+        
+    def on_receive(self, connection_handler, message):
+        self.log("on_receive() -> %s", message)
+        BasicServent.on_receive(self, connection_handler, message)
+        
+    def on_disconnect(self, connection_handler):
+        self.log("on_disconnect()")
+        BasicServent.on_disconnect(self, connection_handler)
+        
+    def on_download(self, event_id, connection_handler):
+        self.log("on_download()")
+        BasicServent.on_download(self, event_id, connection_handler)
+        
+    def on_bootstrap(self, peer_address):
+        self.log("on_bootstrap() -> %s %s" % peer_address)
+        BasicServent.on_bootstrap(self, peer_address)
+        
+    def forward(self, message):
+        self.log("forward() -> %s", message)
+        return BasicServent.forward(self, message)
+    
+    def flood(self, message):
+        self.log("flood() -> %s", message)
+        BasicServent.flood(self, message)

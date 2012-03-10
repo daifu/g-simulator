bd6bfbb03fc4c24e6a612448e31a6cf020610f14
Khai Nguyen <khaing211@gmail.com>
2012-03-10 13:32:56 -0800

    add more logging ping/pong example model after run_servent.py

---
+++ examples/ping_pong_intermediate_example.py
@@ -0,0 +1,29 @@
+from pygnutella.network import create_network
+from pygnutella.demo.log_servent import LogServent
+from pygnutella.messagebody import GnutellaBodyId
+from pygnutella.message import create_message
+import sys
+
+class ExampleServent(LogServent):
+    def on_connect(self, connection_handler):
+        ping_message = create_message(GnutellaBodyId.PING)
+        self.log("sending %s", ping_message)
+        connection_handler.write(ping_message.serialize())        
+        LogServent.on_connect(self, connection_handler)
+        
+        
+def usage():
+    print "Please run with <bootstrap ip> <bootstrap port> <num of nodes>"
+
+def main(args):
+    if len(args)<3:
+        usage();
+        return
+    ip = args[0]
+    port = int(args[1])
+    num_node = int(args[2])
+    address = (ip, port)
+    create_network([ExampleServent]*num_node, address)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])

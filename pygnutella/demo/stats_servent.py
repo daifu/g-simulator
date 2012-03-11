f3c356c3d16569e1d0ca31fd481d75c185feb8b4
Khai Nguyen <khaing211@gmail.com>
2012-03-10 20:45:01 -0800

    this is servent that record number of requested connection, num of accepted connection, num of received byte and num of transmit byte for studying

---
+++ pygnutella/demo/stats_servent.py
@@ -0,0 +1,33 @@
+from ..servent import BasicServent
+
+class LogServent(BasicServent):    
+    def __init__(self, port = 0, files = [], bootstrap_address = None):
+        BasicServent.__init__(self, port, files, bootstrap_address)
+        self.num_requested_conn = 0
+        self.num_accepted_conn = 0
+        self.num_rx_byte = 0
+        self.num_tx_byte = 0
+    
+    def on_accept(self):
+        self.num_requested_conn += 1
+        return BasicServent.on_accept(self)
+    
+    def on_connect(self, connection_handler):
+        self.num_accepted_conn += 1
+        BasicServent.on_connect(self, connection_handler)
+        
+    def on_receive(self, connection_handler, message):
+        self.num_rx_byte += len(message.serialize())
+        BasicServent.on_receive(self, connection_handler, message)
+                
+    def send_message(self, message, handler):
+        self.num_tx_byte += len(message.serialize())
+        BasicServent.send_message(self, message, handler)        
+        
+    def forward(self, message):
+        self.num_tx_byte += len(message.serialize())
+        return BasicServent.forward(self, message)
+    
+    def flood(self, connection_handler, message):
+        self.num_tx_byte += len(message.serialize())*(len(self.reactor.channels)-1)        
+        BasicServent.flood(self, connection_handler, message)

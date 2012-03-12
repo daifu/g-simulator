032c1708e9a6da1cf0bfa23637203f8a9d6ce63c
Khai Nguyen <khaing211@gmail.com>
2012-03-12 10:27:37 -0700

    add doc string on send_message()

--- pygnutella/servent.py
+++ pygnutella/servent.py
@@ -142,6 +142,12 @@ class BasicServent:
         self._logger.debug(msg, *args, **kwargs)
     
     def send_message(self, message, handler):
+        """
+        Servent sends its own message (not as part of flood / forwarding)
+        
+        By using this method, Servent can keep track which message (by message_id) is its own
+        by adding it to ignore dictionary.
+        """
         if message.payload_descriptor == GnutellaBodyId.QUERY or message.payload_descriptor == GnutellaBodyId.PING:
             self.ignore[message.message_id] = time.time()+self.FIXED_EXPIRED_INTERVAL*message.ttl
         handler.write(message.serialize())

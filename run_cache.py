ea49f2ee9f9ff0f374c70b24f597e2dbc1834c97
daifu ye <daifu.ye@gmail.com>
2012-03-21 19:20:40 -0700

    For testing.

--- run_cache.py
+++ run_cache.py
@@ -9,45 +9,40 @@ def usage():
 
 def main(args):
     # create a network
-    if len(args)<3:
-        usage();
-        return
-    ip = args[0]
-    port = int(args[1])
-    num_node = int(args[2])
-    address = (ip, port)
-    create_network([CacheServent]*num_node, address)
-    # node2 = nodes[2]
-    # node0 = nodes[0]
-    # node2.set_files([FileInfo(1,"first file", 600),
-    #                    FileInfo(2,"second file", 2500),
-    #                    FileInfo(3, "third file", 5000)])
-    # node0.query_flood('first file')
+    # if len(args)<3:
+    #     usage();
+    #     return
+    # ip = args[0]
+    # port = int(args[1])
+    # num_node = int(args[2])
+    # address = (ip, port)
+    # create_network([CacheServent]*num_node, address)
+
     # implement the network
-    # servent1 = CacheServent()
-    # servent2 = CacheServent()
-    # servent3 = CacheServent()
-    # servent4 = CacheServent()
-    # servent5 = CacheServent()
-    # servent3.set_files([FileInfo(1,"first file", 600),
-    #                    FileInfo(2,"second file", 2500) ,
-    #                    FileInfo(3, "third file", 5000)])
-    # servent1.reactor.gnutella_connect(servent2.reactor.address)
-    # servent2.reactor.gnutella_connect(servent3.reactor.address)
-    # servent3.reactor.gnutella_connect(servent4.reactor.address)
-    # servent5.reactor.gnutella_connect(servent2.reactor.address)
-    # try:
-    #     scheduler_loop(timeout=1,count=10)
-    # finally:
-    #     #flood is query to neighbors
-    #     servent1.query_flood('first file')
-    #     scheduler_loop(timeout=1,count=10)
-    #     print "============NEXT QUERY============"
-    #     # servent2.query_flood('first file')
-    #     # scheduler_loop(timeout=1,count=10)
-    #     servent5.query_flood('first file')
-    #     scheduler_loop(timeout=1,count=15)
-    #     close_all()
+    servent1 = CacheServent()
+    servent2 = CacheServent()
+    servent3 = CacheServent()
+    servent4 = CacheServent()
+    servent5 = CacheServent()
+    servent3.set_files([FileInfo(1,"first file", 600),
+                       FileInfo(2,"second file", 2500) ,
+                       FileInfo(3, "third file", 5000)])
+    servent1.reactor.gnutella_connect(servent2.reactor.address)
+    servent2.reactor.gnutella_connect(servent3.reactor.address)
+    servent3.reactor.gnutella_connect(servent4.reactor.address)
+    servent5.reactor.gnutella_connect(servent2.reactor.address)
+    try:
+        scheduler_loop(timeout=1,count=10)
+    finally:
+        #flood is query to neighbors
+        servent1.query_flood('first file')
+        scheduler_loop(timeout=1,count=10)
+        print "============NEXT QUERY============"
+        # servent2.query_flood('first file')
+        # scheduler_loop(timeout=1,count=10)
+        servent5.query_flood('first file')
+        scheduler_loop(timeout=1,count=15)
+        close_all()
 
 
 if __name__ == '__main__':
